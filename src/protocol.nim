2d8d34c5e6a749d7fa822e57a4c6b445d5445954
akex <hortinstein@gmail.com>
2022-08-18 22:59:40 -0400

    got encrypted messaging ready for get and post tests

--- src/protocol.nim
+++ src/protocol.nim
@@ -1,23 +1,64 @@
-import json
+import monocypher
+import sysrandom
+import flatty
+import std/base64
+
 type
-  Message* = object
-    username*: string
-    message*: string
+  EncryptedMessage* = object
+    publicKey*: Key
+    nonce*:     Nonce
+    mac*:       Mac
+    cipherLen:  int
+    cipherText: seq[byte]
 
-proc parseMessage*(data: string): Message =
-  let dataJson = parseJson(data)
-  result.username = dataJson["username"].getStr()
-  result.message = dataJson["message"].getStr()
+#[
+  helper function to convert bytes to string
+]# 
+proc toString*(bytes: seq[byte]): string =
+  result = newString(bytes.len)
+  copyMem(result[0].addr, bytes[0].unsafeAddr, bytes.len)
 
-proc createMessage*(username, message: string): string =
-  result = $(%{
-    "username": %username,
-    "message": %message
-  }) & "\c\l"
+#[
+  encrypts a message and returns a structure ready to be serialized and sent
+]#
+proc encryptMessage*( privateKey: Key, 
+                      publicKey: Key, 
+                      plaintext: seq[byte]): EncryptedMessage =
+  #derive the shared key and material needed to encrypt
+  let sharedKey = crypto_key_exchange(privateKey, publicKey)
+  let nonce = getRandomBytes(sizeof(Nonce))
+  #perform encryption
+  let (mac, ciphertext) = crypto_lock(sharedKey, nonce, plaintext)
+  #create the return object
+  let myPubKey = crypto_key_exchange_public_key(privateKey)
+  result = EncryptedMessage(publicKey: myPubKey,
+                              nonce:nonce,
+                              mac:mac,
+                              cipherLen:cipherText.len,
+                              cipherText:cipherText)
+#[
+  decrypts a message and returns a byte array
+]#
+proc decryptMessage*( privateKey: Key, 
+                      encMsg: EncryptedMessage): seq[byte] =
+  #derive the shared key 
+  let sharedKey = crypto_key_exchange(privateKey, encMsg.publicKey)
+  #perform decryption
+  result = crypto_unlock( sharedKey, 
+                          encMsg.nonce, 
+                          encMsg.mac, 
+                          encMsg.ciphertext)
 
-block:
-  let expected = """{"username":"dom","message":"hello"}""" & "\c\l"
-  doAssert createMessage("dom", "hello") == expected
+proc serializeEncryptMessage*(encMsg:EncryptedMessage): string = 
+  result = toFlatty(encMsg)
 
+proc deserializeEncryptMessage*(serEncMsg:string): EncryptedMessage = 
+  result = serEncMsg.fromFlatty(EncryptedMessage)
 
-
+proc base64Str*(msg:string): string = 
+  result = encode(msg,safe=true)
+  
+proc unbase64str*(msg:string): string = 
+  echo msg
+  result = decode(msg)
+  echo result
