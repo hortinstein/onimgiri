4938b01e06b7e76532985fd2b9181ba4c6b6f4e1
Alex <hortinstein@gmail.com>
2022-12-02 14:02:01 -0500

    got replacement working

--- js/yanagiba/main.ts
+++ js/yanagiba/main.ts
@@ -1,11 +1,41 @@
 import { readerFromStreamReader } from "https://deno.land/std@0.167.0/streams/reader_from_stream_reader.ts";
+import config from "../configjs.js";
 
 import { serve } from "https://deno.land/std/http/mod.ts";
 const SAVE_PATH = "./";
 
-export async function carveOut(newConfig: Uint8Array, target: Uint8Array, bin: Uint8Array){
+
+export function findCSA(arr: Uint8Array, subarr: Uint8Array, from_index: number): number {
+  var i = from_index >>> 0,
+    sl = subarr.length,
+    l = arr.length + 1 - sl;
+
+  loop: for (; i < l; i++) {
+    for (var j = 0; j < sl; j++)
+      if (arr[i + j] !== subarr[j])
+        continue loop;
+    return i;
+  }
+  return -1;
+}
+
+export function replaceCSA(arr: Uint8Array, subarr: Uint8Array, index: number): Uint8Array {
+  var i = 0;
+  var newarr = new Uint8Array(arr);
+  while (i < subarr.length) {
+    newarr[index + i] = subarr[i];
+    i++;
+  }
+  return newarr;
+}
+
+export function carveOut(newConfig: Uint8Array, target: Uint8Array, bin: Uint8Array): Uint8Array {
   console.log(newConfig.length, target.length, bin.length);
-} 
+  console.log("bin", bin, "target\n", target, "newConfig\n", newConfig);
+  console.log(findCSA(bin, target, 0));
+  return newConfig;
+
+}
 
 async function reqHandler(req: Request) {
   const url = new URL(req.url);
--- js/yanagiba/main_test.ts
+++ js/yanagiba/main_test.ts
@@ -1,11 +1,31 @@
-import { assertEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
-import { carveOut } from "./main.ts";
+import { assert, assertEquals, assertNotEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
+import { carveOut, findCSA, replaceCSA } from "./main.ts";
 
-Deno.test(function testCarve() {
-  return new Promise(async () => {
+Deno.test(function testFindCSA() {
+  return new Promise(async (resolve, reject) => {
     const bin = await Deno.readFile('../../bin/agent');
     const target = await Deno.readFile('../../debug.config');
     const newConfig = await Deno.readFile('../../release.config');
-    carveOut(newConfig, target, bin);
+
+    assert(findCSA(bin, target, 0) !== -1, "substring was not found in binary");
+
+    resolve();
+  })
+});
+
+Deno.test(function testRepCSA() {
+  return new Promise(async (resolve, reject) => {
+    const bin = await Deno.readFile('../../bin/agent');
+    const target = await Deno.readFile('../../debug.config');
+    const newConfig = await Deno.readFile('../../release.config');
+
+    const index = findCSA(bin, target, 0)
+    const newBin = replaceCSA(bin, newConfig, index)
+    assertNotEquals(newBin, bin, "binary was not replaced");
+
+    let difference = bin.filter(x => !newBin.includes(x));
+    console.log(difference);
+    await Deno.writeFile('../../bin/newagent', newBin)
+    resolve();
   })
 });
